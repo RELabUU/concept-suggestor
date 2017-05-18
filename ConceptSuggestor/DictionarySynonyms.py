9f39c5887e02b135dd099ccdcb86aaa4dcbfffc2
Raphaël Claasen <raphael.claasen@gmail.com>
2017-05-18 14:40:16 +0200

    Two-way dictionary synonym check now only requests synonyms of the new word once.

--- ConceptSuggestor/DictionarySynonyms.py
+++ ConceptSuggestor/DictionarySynonyms.py
@@ -10,20 +10,24 @@ class DictionarySynonyms(object):
 
     # Checks if the collection has a synonym of the word
     def HasSynonym(self, collection, word):
+        # Get the synonyms of "word" once and store them
+        self.synonymsOfWord = []
+        if(self.TWOWAYCHECK is True):
+            self.synonymsOfWord = self.dict.synonym(word)
+            print("Synonyms of %s: %s" % (word, self.synonymsOfWord)) # DEBUG
+
         for cword in collection:
-            if self.IsSynonym(cword, word):
+            if self.IsSynonym(cword, word, self.synonymsOfWord):
                 return True
         return False
 
     # Checks if wordB is a synonym of wordA, and vice-versa if TWOWAYCHECK == True
-    def IsSynonym(self, wordA, wordB):
+    def IsSynonym(self, wordA, wordB, synonymsOfWordB):
         synonyms = self.dict.synonym(wordA)
         print("Synonyms of %s: %s" % (wordA, synonyms)) # DEBUG
         result = wordB.lower() in synonyms
         if result is True:
             return result
         elif self.TWOWAYCHECK is True:
-            synonyms = self.dict.synonym(wordB)
-            print("Synonyms of %s: %s" % (wordB, synonyms)) # DEBUG
-            result = wordA.lower() in synonyms
+            result = wordA.lower() in synonymsOfWordB
         return result
