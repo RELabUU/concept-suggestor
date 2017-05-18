ddf72366232ac681539f22c6bb1a2746226de053
Raphaël Claasen <raphael.claasen@gmail.com>
2017-05-18 14:39:50 +0200

    Asks for program mode so that not everything always loads.

--- ConceptSuggestor/ConceptSuggestor.py
+++ ConceptSuggestor/ConceptSuggestor.py
@@ -1,16 +1,43 @@
-from SynonymRemover import SynonymRemover
-from DictionarySynonyms import DictionarySynonyms
-
 # The concepts that are already in the model.
 concepts = ["Aircraft", "Flight", "Gate", "Ticket", "Airspace", "Pilot", "Security", "Route"]
-
 print("Concepts already in the model: %s" % concepts)
+
+
+def IsValidChoice(choice):
+    if(choice=="a" or choice=="b"):
+        return True
+    else:
+        return False
+
+def GetProgramMode():
+    print("These are your options:")
+    print("a - check similarity using word vectors")
+    print("b - check whether a synonym exists using thesaurus.com")
+
+    # Get the program mode from the user and ensure it's valid.
+    choice = input("Please tell me what to do (type a letter): ").lower()
+    if(IsValidChoice(choice)):
+        return choice
+    else:
+        print("I could not understand that option. Please try again.")
+        return GetProgramMode()
+
+    return choice
+
+choice = GetProgramMode()
 new = input("Enter a concept to be checked for similarity: ")
 
-print("Result of semantic similarity: ")
-sr = SynonymRemover(concepts)
-print(sr.HasSynonym(new))
+if(choice == "a"):
+    from SynonymRemover import SynonymRemover
+    print("Result of semantic similarity: ")
+    sr = SynonymRemover(concepts)
+    print(sr.HasSynonym(new))
 
-print("Result of Thesaurus.com synonyms: ")
-ds = DictionarySynonyms()
-print(ds.HasSynonym(concepts, new))
+elif(choice=="b"):
+    from DictionarySynonyms import DictionarySynonyms
+    print("Result of Thesaurus.com synonyms: ")
+    ds = DictionarySynonyms()
+    print(ds.HasSynonym(concepts, new))
+
+else:
+    print("Invalid mode. Aborting.")
