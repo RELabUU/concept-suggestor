e1e5e15e11c0698693fb135f530c48143f637e97
Raphaël Claasen <raphael.claasen@gmail.com>
2017-07-07 12:27:44 +0200

    Can now set settings from inside the program as well.

--- ConceptSuggestor/Settings.py
+++ ConceptSuggestor/Settings.py
@@ -16,6 +16,27 @@ class Settings(object):
             with open(file) as data_file:
                 self.data = json.load(data_file)
 
+            # Set the variables to the values found in the JSON file.
+            self.reload = self._Setting("reload")
+            self.wordnetSimilarityMethod = self._Setting("wordnetSimilarityMethod")
+            self.useSynonymity = self._Setting("useSynonymity")
+            self.spacyWeight = self._Setting("spacyWeight")
+            self.wordnetWeight = self._Setting("wordnetWeight")
+            self.similarityThreshold = self._Setting("similarityThreshold")
+            self.alpha = self._Setting("alpha")
+            self.beta = self._Setting("beta")
+            self.gamma = self._Setting("gamma")
+            self.delta = self._Setting("delta")
+            self.epsilon = self._Setting("epsilon")
+            self.minLambda = self._Setting("minLambda")
+            self.maxLambda = self._Setting("maxLambda")
+            self.lambdaSteps = self._Setting("lambdaSteps")
+            self.lambdaPlusMu = self._Setting("lambdaPlusMu")
+            self.minGamma = self._Setting("minGamma")
+            self.maxGamma = self._Setting("maxGamma")
+            self.gammaSteps = self._Setting("gammaSteps")
+            self.gammaPlusDeltaPlusTwoEpsilon = self._Setting("gammaPlusDeltaPlusTwoEpsilon")
+
         def _Setting(self, name):
             return self.data[name]
 
@@ -30,67 +51,139 @@ class Settings(object):
     def LoadSettings(self, file = SETTINGS_PATH):
         Settings.instance._LoadSettings()
 
-    def _Setting(self, name):
-        return Settings.instance._Setting(name)
-
-    # Methods to directly get all the various settings instead of having to manually input the names.
+    # Methods to directly get and set all the various settings.
     @property
     def Reload(self):
-        return self._Setting("reload")
+        return Settings.instance.reload
+    @Reload.setter
+    def Reload(self, value):
+        Settings.instance.reload = value
+
     @property
     def WordNetSimilarityMethod(self):
-        return self._Setting("wordnetSimilarityMethod")
+        return Settings.instance.wordnetSimilarityMethod
+    @WordNetSimilarityMethod.setter
+    def WordNetSimilarityMethod(self, value):
+        Settings.instance.wordnetSimilarityMethod = value
+
     @property
     def UseSynonymity(self):
-        return self._Setting("useSynonymity")
+        return Settings.instance.useSynonymity
+    @UseSynonymity.setter
+    def UseSynonymity(self, value):
+        Settings.instance.useSynonymity = value
+
     @property
     def SpaCyWeight(self):
-        return self._Setting("spacyWeight")
+        return Settings.instance.spacyWeight
+    @SpaCyWeight.setter
+    def SpaCyWeight(self, value):
+        Settings.instance.spacyWeight = value
+
     @property
     def WordNetWeight(self):
-        return self._Setting("wordnetWeight")
+        return Settings.instance.wordnetWeight
+    @WordNetWeight.setter
+    def WordNetWeight(self, value):
+        Settings.instance.wordnetWeight = value
+
     @property
     def SimilarityThreshold(self):
-        return self._Setting("similarityThreshold")
+        return Settings.instance.similarityThreshold
+    @SimilarityThreshold.setter
+    def SimilarityThreshold(self, value):
+        Settings.instance.similarityThreshold = value
+
     @property
     def Alpha(self):
-        return self._Setting("alpha")
+        return Settings.instance.alpha
+    @Alpha.setter
+    def Alpha(self, value):
+        Settings.instance.alpha = value
+
     @property
     def Beta(self):
-        return self._Setting("beta")
+        return Settings.instance.beta
+    @Beta.setter
+    def Beta(self, value):
+        Settings.instance.beta = value
+
     @property
     def Gamma(self):
-        return self._Setting("gamma")
+        return Settings.instance.gamma
+    @Gamma.setter
+    def Gamma(self, value):
+        Settings.instance.gamma = value
+
     @property
     def Delta(self):
-        return self._Setting("delta")
+        return Settings.instance.delta
+    @Delta.setter
+    def Delta(self, value):
+        Settings.instance.delta = value
+
     @property
     def Epsilon(self):
-        return self._Setting("epsilon")
+        return Settings.instance.epsilon
+    @Epsilon.setter
+    def Epsilon(self, value):
+        Settings.instance.epsilon = value
+
     @property
     def MinLambda(self):
-        return self._Setting("minLambda")
+        return Settings.instance.minLambda
+    @MinLambda.setter
+    def MinLambda(self, value):
+        Settings.instance.minLambda = value
+
     @property
     def MaxLambda(self):
-        return self._Setting("maxLambda")
+        return Settings.instance.maxLambda
+    @MaxLambda.setter
+    def MaxLambda(self, value):
+        Settings.instance.maxLambda = value
+
     @property
     def LambdaSteps(self):
-        return self._Setting("lambdaSteps")
+        return Settings.instance.lambdaSteps
+    @LambdaSteps.setter
+    def LambdaSteps(self, value):
+        Settings.instance.lambdaSteps = value
+
     @property
     def LambdaPlusMu(self):
-        return self._Setting("lambdaPlusMu")
+        return Settings.instance.lambdaPlusMu
+    @LambdaPlusMu.setter
+    def LambdaPlusMu(self, value):
+        Settings.instance.lambdaPlusMu = value
+
     @property
     def MinGamma(self):
-        return self._Setting("minGamma")
+        return Settings.instance.minGamma
+    @MinGamma.setter
+    def MinGamma(self, value):
+        Settings.instance.minGamma = value
+
     @property
     def MaxGamma(self):
-        return self._Setting("maxGamma")
+        return Settings.instance.maxGamma
+    @MaxGamma.setter
+    def MaxGamma(self, value):
+        Settings.instance.maxGamma = value
+
     @property
     def GammaSteps(self):
-        return self._Setting("gammaSteps")
+        return Settings.instance.gammaSteps
+    @GammaSteps.setter
+    def GammaSteps(self, value):
+        Settings.instance.gammaSteps = value
+
     @property
     def GammaPlusDeltaPlusTwoEpsilon(self):
-        return self._Setting("gammaPlusDeltaPlusTwoEpsilon")
+        return Settings.instance.gammaPlusDeltaPlusTwoEpsilon
+    @GammaPlusDeltaPlusTwoEpsilon.setter
+    def GammaPlusDeltaPlusTwoEpsilon(self, value):
+        Settings.instance.gammaPlusDeltaPlusTwoEpsilon = value
 
     # Derived settings
     @property
